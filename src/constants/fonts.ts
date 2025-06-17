2ba1f5552914db85f1a9c899d4108403ecf01692
taylormohit9997@gmail.com <>
2025-06-18 02:08:11 +0530

    init commit

--- src/constants/fonts.ts
+++
@@ -1,9 +0,0 @@
-
-export const fontFamilies = {
-  HUNINN: {
-    normal: 'HuninnRegular',
-    medium: 'HuninnRegular',
-    bold: 'HuninnRegular',
-  },
-  // Adjust the above code to fit your chosen fonts' names
-};
